4596f2374cac6c2a8bbac6b9059262c17b0c490b
fangjjcs <grace963214789@gmail.com>
2022-01-16 12:56:24 +0800

    update NavLinks

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,19 +1,40 @@
 import React, {useState} from "react";
 import { Link, NavLink } from "react-router-dom";
-
 import MainHeader from "./MainHeader";
 import './MainNavigation.css';
 import NavLinks from "./NavLinks"; 
 import SideDrawer from "./SideDrawer";
 import BackDrop from "../Backdrop/Backdrop"
 import Footer from "../Footer/Footer";
+import { Avatar, Drawer } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    small: {
+      width: theme.spacing(4),
+      height: theme.spacing(4),
+      fontSize: "1rem",
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+  }));
+
 
 const MainNavigation = props => {
     
+    const classes = useStyles();
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
     const openDrawer =()=>{
-        setDrawerIsOpen(true);
+        setDrawerIsOpen(!drawerIsOpen);
     }
 
     const closeDrawer =()=>{
@@ -22,10 +43,16 @@ const MainNavigation = props => {
 
     return (
         <React.Fragment>
-            {drawerIsOpen? <BackDrop onClick={closeDrawer}/>: null }
-            <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+            {/* <Drawer open={drawerIsOpen} onClose={closeDrawer}>
                 <nav className="main-navigation__drawer-nav">
-                    <NavLinks/>
+                    <NavLinks onClose={closeDrawer}/>
+                    <Footer/>
+                </nav>
+            </Drawer> */}
+            {drawerIsOpen? <BackDrop onClick={closeDrawer}/>: null }
+            <SideDrawer show={drawerIsOpen} >
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks onClose={closeDrawer}/>
                     <Footer/>
                 </nav>
             </SideDrawer>
@@ -38,12 +65,8 @@ const MainNavigation = props => {
                 <h2 className="main-navigation__title">
                     <Link to="/">{props.config.headerName}</Link>
                 </h2>
-                <nav className="main-navigation__header-nav-auth">
-                    <ul className="nav-links">
-                        <li>
-                            <NavLink to="/login">Login</NavLink>
-                        </li>
-                    </ul>
+                <nav className="main-navigation__header-nav-avatar">
+                    <Avatar className={classes.small}>N</Avatar>
                 </nav>
             </MainHeader>
         </React.Fragment>
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -4,17 +4,17 @@ import { NavLink } from "react-router-dom";
 import './NavLinks.css';
 
 const NavLinks = props => {
-    return <ul className="nav-links">
-        <li>
+    return <ul className="nav-links" >
+        <li onClick={props.onClose}> 
             <NavLink to="/" exact>Home</NavLink>
         </li>
-        <li>
+        <li onClick={props.onClose}>
             <NavLink to="/movies">Movies</NavLink>
         </li>
-        <li>
+        <li onClick={props.onClose}>
             <NavLink to="/genres">Genres</NavLink>
         </li>
-        <li>
+        <li onClick={props.onClose}>
             <NavLink to="/add-movie">Add movie</NavLink>
         </li>
     </ul>
